e913f48dd93a83bcbdf555b5d143df4a62f36cec
Twinkle942910 <akarpun94@gmail.com>
2016-03-20 18:08:53 +0200

    Configure project. Creating base controller. Sever ok

---
+++ src/main/java/com/twinkle/retwin/server/controller/ReminderController.java
@@ -0,0 +1,21 @@
+package com.twinkle.retwin.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/retwin")
+public class ReminderController
+{
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getReminder(ModelMap model)
+    {
+        return "My reminder";
+    }
+
+}
