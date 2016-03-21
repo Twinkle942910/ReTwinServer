a7268e48bca8f39ad9f004a2838d55788cd63d66
Twinkle942910 <akarpun94@gmail.com>
2016-03-21 02:13:39 +0200

    Configure Rest Service

--- src/main/java/com/twinkle/retwin/server/controller/ReminderController.java
+++ src/main/java/com/twinkle/retwin/server/controller/ReminderController.java
@@ -1,21 +1,32 @@
 package com.twinkle.retwin.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.twinkle.retwin.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/retwin")
 public class ReminderController
 {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap model)
+    public Remind getReminder()
     {
-        return "My reminder";
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("My first remind");
+
+        return remind;
     }
 
 }
